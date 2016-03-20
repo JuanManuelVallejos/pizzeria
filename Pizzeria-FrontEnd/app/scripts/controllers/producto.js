f57eff1b84198b4fdf2aa626fdb3764639f9a25f
fernandodm <dimegliofernando@gmail.com>
2016-03-19 23:07:02 -0300

    se agrego agregar producto en el front, se modifico metodos en el cliente dao para que utilicen criterias y se agrego metodo en pedido

--- Pizzeria-FrontEnd/app/scripts/controllers/producto.js
+++ Pizzeria-FrontEnd/app/scripts/controllers/producto.js
@@ -14,13 +14,34 @@ angular.module('pizzeriaFrontEndApp')
       'AngularJS',
       'Karma'
     ];
+	
+	$scope.crearProducto = function() {
+       // $http.post('http://localhost:8080/Pizzeria-Backend/rest/productos/crear', {nombre: $scope.nombreProducto, precio: $scope.precioProducto}).success(function (data) {     			
+		//})
+		
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8080/Pizzeria-Backend/rest/productos/crear',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            transformRequest: function(obj) {
+                var str = [];
+                    for(var p in obj)
+                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+            return str.join("&");
+            },
 
-	this.cargarProductos = function(){
+            data: {nombre: $scope.nombreProducto, precio: $scope.precioProducto}
+
+        }).success(function (data) {});
+		
+    }
+
+	$scope.cargarProductos = function(){
 		$http.get('http://localhost:8080/Pizzeria-Backend/rest/productos/listar').success(function (data) {
         	$scope.productos = data;
     	});
 	}
 
-	this.cargarProductos();
+	$scope.cargarProductos();
 
   });
