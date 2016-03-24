a47c78eabf0af7a686e2ab96a3e5041fd2b4e38a
fernando <dimegliofernando@gmail.com>
2016-03-24 00:33:22 -0300

    se agrego campos del login con el boton, se comenzon con el ingresar en el login.js, se agrego metodo ingresar en el clienteRest, (este metodo no anda tira el error que tiraba antes al crear un cliente), se arreglo crear cliente, agregado de password al cliente

--- Pizzeria-FrontEnd/app/scripts/controllers/login.js
+++ Pizzeria-FrontEnd/app/scripts/controllers/login.js
@@ -8,10 +8,34 @@
  * Controller of the pizzeriaFrontEndApp
  */
 angular.module('pizzeriaFrontEndApp')
-  .controller('LoginCtrl', function () {
+  .controller('LoginCtrl', function ($http, $scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+     $scope.ingresar = function() {
+     	$http({
+            method: 'POST',
+            url: 'http://localhost:8080/Pizzeria-Backend/rest/clientes/ingresar',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            transformRequest: function(obj) {
+                var str = [];
+                    for(var p in obj)
+                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+            return str.join("&");
+            },
+
+            data: {usuario: $scope.usuario, password: $scope.contrasenia}
+
+        }).success(function (data) {
+        	if(data == -1){
+        		alerte("El numero de cliente o password son incorrectos")
+        	}else{
+        		//redireccionar
+        	}     	
+        });
+     };
+
   });
